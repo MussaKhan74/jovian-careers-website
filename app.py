0d8a005872f6c7358d9b81455681b257acbc1e39
Mussa Khan Shaukat <MarkeLoof@gmail.com>
2023-03-22 00:30:13 +0000

    added Bootstrap

--- app.py
+++ app.py
@@ -3,6 +3,33 @@ from flask import Flask, render_template
 app = Flask(__name__)
 print(__name__)
 
+JOBS = [
+  {
+    'id': 1,
+    'title': 'Data Analyst',
+    'location': 'Lahore, Pakistan',
+    'salary': 'Rs. 15,00,00'
+  },
+  {
+    'id': 2,
+    'title': 'Data Scientist',
+    'location': 'Lahore, Pakistan',
+    'salary': 'Rs. 25,00,000'
+  },
+  {
+    'id': 3,
+    'title': 'Backend Engineer',
+    'location': 'San Francisco, USA',
+    'salary': '$120,000'
+  },
+  {
+    'id': 3,
+    'title': 'Frontend Engineer',
+    'location': 'Remote',
+    'salary': '$100,000'
+  },
+]
+
 
 @app.route("/")
 def hello_worl():
